6fb1aa9af5183dd95cd1521bc4f5d0ebf0245ee7
SamyCode <sambea2103@gmail.com>
2024-01-20 21:57:55 -0500

    feat(SEO): add favicon with config dark/light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,19 @@ import '@/styles/globals.css'
 
 export const metadata: Metadata = {
   title: 'Samotion',
-  description: 'Best & Open Source Notion alternative'
+  description: 'Best & Open Source Notion alternative',
+  icons: [
+    {
+      media: '(prefers-color-scheme: light)',
+      url: '/logo.svg',
+      href: '/logo.svg'
+    },
+    {
+      media: '(prefers-color-scheme: dark)',
+      url: '/logo-dark.svg',
+      href: '/logo-dark.svg'
+    }
+  ]
 }
 
 export default function RootLayout ({
