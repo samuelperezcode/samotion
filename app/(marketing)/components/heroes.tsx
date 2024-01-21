f24a900c95af49dd7aaa4f18b9551a5cb2af17e6
SamyCode <sambea2103@gmail.com>
2024-01-21 18:45:16 -0500

    fix(theme): add right assests depending of selected theme

--- app/(marketing)/components/heroes.tsx
+++ app/(marketing)/components/heroes.tsx
@@ -1,12 +1,15 @@
+'use client'
+import { useTheme } from 'next-themes'
 import Image from 'next/image'
 
 export function Heroes () {
+  const { resolvedTheme } = useTheme()
   return (
     <section className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
         <div className='relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]'>
           <Image
-            src='/documents.png'
+            src={resolvedTheme === 'dark' ? '/documents-dark.png' : '/documents.png' }
             alt='Document illustrative image'
             fill
             className= 'object-contain'
@@ -14,7 +17,7 @@ export function Heroes () {
         </div>
         <div className='relative h-[400px] w-[400px] hidden md:block'>
           <Image
-            src='/reading.png'
+            src={resolvedTheme === 'dark' ? '/reading-dark.png' : '/reading.png'}
             alt='Document illustrative image'
             fill
             className= 'object-contain'
--- app/(marketing)/components/logo.tsx
+++ app/(marketing)/components/logo.tsx
@@ -1,12 +1,16 @@
+'use client'
+
 import Image from 'next/image'
 import { poppins } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
+import { useTheme } from 'next-themes'
 
 export function Logo () {
+  const { resolvedTheme } = useTheme()
   return (
     <div className="hidden md:flex items-center gap-x-2">
       <Image
-        src={'/logo.svg'}
+        src={resolvedTheme === 'dark' ? '/logo-dark.svg' : '/logo.svg'}
         alt="Samotion logo"
         width={40}
         height={40}
